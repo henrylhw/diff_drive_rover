96a4b6b8770d532a20e695c0988a8125a08c0448
Henry <henrylhw@connect.hku.hk>
2023-08-28 14:20:59 +0800

    update ros-workspace config

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/innowingintern/diff_drive_rover/ros-workspace/src
+# Build directory: /home/innowingintern/diff_drive_rover/ros-workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("differential-drive")
+subdirs("bringup")
---
+++ build/differential-drive/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/innowingintern/diff_drive_rover/ros-workspace/src/differential-drive
+# Build directory: /home/innowingintern/diff_drive_rover/ros-workspace/build/differential-drive
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
